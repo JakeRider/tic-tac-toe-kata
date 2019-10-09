8c35439487b794f5436e4f721d72538980201880
Jake Rider <hello@jakerider.io>
2019-10-09 19:55:56 -0400

    Game should render Board

--- src/components/Game/Game.spec.tsx
+++ src/components/Game/Game.spec.tsx
@@ -1,12 +1,17 @@
 import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
+import Board from '../Board';
 import Game from './Game';
 
-describe('<App />', () => {
+describe('<Game />', () => {
   let wrapper: ShallowWrapper;
   beforeEach(() => (wrapper = shallow(<Game />)));
 
   it('should render a `<div />`', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render `<Board />`', () => {
+    expect(wrapper.containsMatchingElement(<Board />)).toEqual(true);
   });
 });

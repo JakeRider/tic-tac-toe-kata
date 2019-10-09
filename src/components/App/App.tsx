f507d3e7a462176ec9d71a0a42341e3b19f0b11b
Jake Rider <hello@jakerider.io>
2019-10-09 19:45:54 -0400

    Render Game in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,16 @@
 import classNames from 'classnames/bind';
 import React from 'react';
+import Game from '../Game';
 import styles from './App.module.css';
 
 const cx = classNames.bind(styles);
 
 function App(): JSX.Element {
-  return <div className={cx('app')}>Hello, World!</div>;
+  return (
+    <div className={cx('app')}>
+      <Game />
+    </div>
+  );
 }
 
 export default App;

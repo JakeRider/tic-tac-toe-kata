5b59b6438383177407d26a1f67b7b02e270cdce6
Jake Rider <hello@jakerider.io>
2019-10-09 20:49:27 -0400

    Game renders 2 MoveList

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Board from '../Board';
 import InfoPrompter from '../InfoPrompter';
+import MoveList from '../MoveList';
 
 function Game(): JSX.Element {
   return (
     <div>
       <InfoPrompter />
       <Board />
+      <MoveList />
+      <MoveList />
     </div>
   );
 }

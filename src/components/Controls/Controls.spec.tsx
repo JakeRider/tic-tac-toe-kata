ce7d2b6eb6e49ff29ead0cc3c66c9c67ccc146d1
Jake Rider <hello@jakerider.io>
2019-10-10 09:28:27 -0400

    Controls should handle clicks

--- src/components/Controls/Controls.spec.tsx
+++ src/components/Controls/Controls.spec.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 import Controls from './Controls';
 
 describe('<Controls />', () => {
+  const rewindAction = jest.fn();
+  const resetAction = jest.fn();
   let wrapper: ShallowWrapper;
-  beforeEach(() => (wrapper = shallow(<Controls />)));
+  beforeEach(
+    () =>
+      (wrapper = shallow(
+        <Controls rewindAction={rewindAction} resetAction={resetAction} />
+      ))
+  );
 
   it('should render a `<div />`', () => {
     expect(wrapper.find('div').length).toEqual(1);
@@ -12,5 +19,15 @@ describe('<Controls />', () => {
 
   it('should contain two buttons', () => {
     expect(wrapper.find('button').length).toBe(2);
+  });
+
+  it('should handle a click for each button', () => {
+    rewindAction.mockClear();
+    resetAction.mockClear();
+    wrapper.find('button').forEach(button => {
+      button.simulate('click');
+    });
+    expect(rewindAction).toBeCalled();
+    expect(resetAction).toBeCalled();
   });
 });

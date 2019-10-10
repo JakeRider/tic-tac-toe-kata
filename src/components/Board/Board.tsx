31f6cedb5f80c2bd74b1e5a81a800ae76f8312bd
Jake Rider <hello@jakerider.io>
2019-10-09 20:29:33 -0400

    Create styles for Board & Square

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,11 +1,15 @@
+import classNames from 'classnames/bind';
 import React from 'react';
 import Square from '../Square';
+import styles from './Board.module.css';
+
+const cx = classNames.bind(styles);
 
 function Board(): JSX.Element {
   const range = [...Array(9).keys()];
 
   return (
-    <div>
+    <div className={cx('board')}>
       {range.map(index => (
         <Square key={index} />
       ))}
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,7 +1,11 @@
+import className from 'classnames/bind';
 import React from 'react';
+import styles from './Square.module.css';
+
+const cx = className.bind(styles);
 
 function Square(): JSX.Element {
-  return <div>Square</div>;
+  return <div className={cx('square')}>Square</div>;
 }
 
 export default Square;

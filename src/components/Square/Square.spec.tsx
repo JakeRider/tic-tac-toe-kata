7f115ad3fa5a2b3aa3c336661a3f627414aa5eca
Jake Rider <hello@jakerider.io>
2019-10-09 22:02:51 -0400

    Square should have an "X" state

--- src/components/Square/Square.spec.tsx
+++ src/components/Square/Square.spec.tsx
@@ -1,17 +1,22 @@
-import { mount, shallow, ShallowWrapper } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import React from 'react';
 import Square from './Square';
 
 describe('<Square />', () => {
-  let wrapper: ShallowWrapper;
-  beforeEach(() => (wrapper = shallow(<Square />)));
+  let wrapper: ReactWrapper;
+  beforeEach(() => (wrapper = mount(<Square />)));
 
   it('should render `<div />`', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
 
   it('should default to an empty state', () => {
-    const contents = mount(<Square />).prop('contents');
+    const contents = wrapper.prop('contents');
     expect(contents).toBe(null);
+  });
+
+  it('should have an "X" state', () => {
+    wrapper.setProps({ contents: 'X' });
+    expect(wrapper.text()).toBe('X');
   });
 });

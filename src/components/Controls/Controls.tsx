57f05e2d4f1d67b1ab41145883c36780d6c9a913
Jake Rider <hello@jakerider.io>
2019-10-09 21:17:44 -0400

    Create controls styles

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,7 +1,11 @@
+import classNames from 'classnames/bind';
 import React from 'react';
+import styles from './Controls.module.css';
+
+const cx = classNames.bind(styles);
 
 function Controls(): JSX.Element {
-  return <div>This is the Controls.</div>;
+  return <div className={cx('controls')}>This is the Controls.</div>;
 }
 
 export default Controls;
